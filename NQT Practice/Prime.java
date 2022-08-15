af288e2d04d348237fe00bcbce0c54f5eb9316de
Rajorshi Ghosh <rajorocco@gmail.com>
2022-08-15 19:17:22 +0000

    gitpod test commit

---
+++ NQT Practice/Prime.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean flag = false;
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
+            System.out.println("NOT Prime");
+        } else {
+            System.out.println("Prime");
+        }
+        sc.close();
+    }
+}
---
+++ NQT Practice/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n - 1; i++) {
+            for (int j = i; j <= n - 1; j++) {
+                System.out.print(" ");
+            }
+            
+            System.out.println("");
+        }
+    }
+}
